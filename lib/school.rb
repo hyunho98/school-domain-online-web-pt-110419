4b3ba559c0cc60b72a25177329acbfa496cde935
Alex Lee <hyunho98@gmail.com>
2019-11-30 01:04:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,8 +3,15 @@ class School
 
   @roster = {}
 
-  def initialize (name)
+  def initialize(name)
     @name = name
+  end
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+    end
+    roster[grade] = name
   end
 
 
