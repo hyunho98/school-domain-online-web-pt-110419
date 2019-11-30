cccfac404ef448a7f28ece7788710e19afcf9921
Alex Lee <hyunho98@gmail.com>
2019-11-30 01:07:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,11 +8,17 @@ class School
   end
 
   def add_student(name, grade)
-    if roster[grade] == nil
-      roster[grade] = []
+    if @roster[grade] == nil
+      @roster[grade] = []
     end
-    roster[grade] = name
+    @roster[grade] = name
   end
 
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each{|index, value| value.sort}
 
 end
